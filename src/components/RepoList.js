6d103c9efd4e9d4783dc8a966974256d8208114b
Arnon <nonthegenocide@me.com>
2019-10-29 16:25:04 +0700

    Add: Styling and make responsive

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -2,13 +2,14 @@ import React, {Fragment} from "react";
 import { PropTypes } from "prop-types";
 
 const RepoList = ({
+  // Define All Props
   avatar,
   owner,
+  ownerProfile,
   name,
   forks,
   language,
   description,
-  issues,
   stars,
   url,
   createdAt
@@ -16,24 +17,28 @@ const RepoList = ({
   return (
     <Fragment>
       <div className="repo-avatar">
-        <img src={avatar} alt="avatar" />
+        <img className="repo-avatar-item" src={avatar} alt="avatar" />
       </div>
       <div className="repo-information">
         <h2 className="repo-name">
-          <a href={url} target="_blank" rel="noreferrer noopener">
+          <a className="repo-link" href={url} target="_blank" rel="noreferrer noopener">
             {name}
           </a>
         </h2>
         <p className="repo-description">{description}</p>
         <div>
           <div className="repo-stars">
-            <span>Stars {stars}</span>
+            <span>Stars: {stars}</span>
           </div>
-          <div className="repo-issues">
-            <span>Issues {issues}</span>
+          <div className="repo-forks">
+            <span>Forks: {forks}</span>
+          </div>
+          <div className="repo-lang">
+            <span>Language: {language}</span>
           </div>
           <div className="repo-submitted">
-            {`Submitted ${createdAt.split("T")[0]} by ${owner}`}
+            {`Submitted : ${createdAt.split("T")[0]} by `}
+            <a className="repo-ownerProfile" rel="noopener noreferrer" href={ownerProfile} target="_blank">{owner}</a>
           </div>
         </div>
       </div>
@@ -41,16 +46,18 @@ const RepoList = ({
   );
 };
 
-// type checking for our props via propTypes.
+// Type checking for all props via propTypes.
 RepoList.propTypes = {
   avatar: PropTypes.string.isRequired,
   owner: PropTypes.string.isRequired,
+  ownerProfile: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
-  issues: PropTypes.number.isRequired,
+  forks: PropTypes.number.isRequired,
   stars: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired
+  createdAt: PropTypes.string.isRequired,
+  language: PropTypes.string,
 };
 
 export default RepoList;
