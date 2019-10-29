481ce42fc95a86b0fcfc213540364ff9e727cf68
Arnon <nonthegenocide@me.com>
2019-10-29 15:57:42 +0700

    Update: Re-arrage assets

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,14 @@
-import React, { Fragment } from 'react';
-import loading from '../loader.svg'
+import React from 'react';
+import Loading from '../static/loader.svg'
 
 function Loader() {
   return(
     <div className="loader">
       <h1>Loading...</h1>
-      <img src={loading} width="100"/>
+      <img src={Loading} width="100" alt="Loading"/>
     </div>
   )
 }
 
 export default Loader;
+ 
