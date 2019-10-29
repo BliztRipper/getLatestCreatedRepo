485abe554ffd14d40e23f6073aa102bbadbd1d3d
Arnon <nonthegenocide@me.com>
2019-10-29 15:57:25 +0700

    Add: React helmet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.scss';
 import GithubIcon from "./static/GitHub-Mark.png"
 import Repositories from "./components/Repositories"
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Most Stared - Rock Star Repositories</title>
+      </Helmet>
+      <div className="App">
       <div className="navbar">
         <img className="icon" src={GithubIcon} alt="icon"/>
       </div>
       <Repositories/>
     </div>
+    </Fragment>
   );
 }
 
