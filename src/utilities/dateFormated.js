d69f67e051cc553653968dfd8d37183065bb1579
Arnon <nonthegenocide@me.com>
2019-10-29 15:57:58 +0700

    Add: comments

--- src/utilities/dateFormated.js
+++ src/utilities/dateFormated.js
@@ -1,7 +1,10 @@
 
 export const dateFormated = () => {
+  // Get Today Date
   const todayDate = new Date();
+  // Get 30 days before today
   todayDate.setDate(todayDate.getDate() - 30);
+  // Get only YYY-MM-DD
   const lastThirtyDay = todayDate.toJSON().split("T")[0];
   return lastThirtyDay;
 }
